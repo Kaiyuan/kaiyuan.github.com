32543c3d2ad2b825875ab9edab6fb6a1e1e84a88
Kaiyuan <sealour@hotmail.com>
2013-03-15 12:30:34 +0800

    add footnotes.js

---
+++ footnotes.js/script.js
@@ -0,0 +1,21 @@
+$('sup').hover(
+	function () {
+		var suptext = $(this).data("text");
+		$("body").append('<div class="sup-box">'+suptext+'</div>' );
+		var supx = $(this).offset().left;
+		var supy = $(this).offset().top;
+		// var suph = $(this).height();
+		var divx = $('.sup-box').width();
+		var divy = $('.sup-box').height();
+		$('.sup-box').css('left', supx-divx/2);
+		$('.sup-box').css('top', supy-divy-30);
+		$('.sup-box').hide();
+		$('.sup-box').fadeIn("fast");
+	},
+	function () {
+		$('.sup-box').fadeOut("fast");
+		setTimeout(function () {
+			$('.sup-box').detach();
+		},100);
+	}
+);
